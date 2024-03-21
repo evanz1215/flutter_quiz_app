44bd3afaad69935f2b8bda40b21aeb060bb26da9
Evan <evanz1215@gmail.com>
2024-03-21 23:23:21 +0800

    調整圖案透明度&按鈕樣式

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
@@ -12,7 +13,15 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             "assets/images/quiz-logo.png",
             width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
+          // Opacity(
+          //   opacity: 0.6,
+          //   child: Image.asset(
+          //     "assets/images/quiz-logo.png",
+          //     width: 300,
+          //   ),
+          // ),
           const SizedBox(
             height: 80,
           ),
@@ -26,12 +35,13 @@ class StartScreen extends StatelessWidget {
           const SizedBox(
             height: 30,
           ),
-          OutlinedButton(
+          OutlinedButton.icon(
               onPressed: () {},
               style: OutlinedButton.styleFrom(
                 foregroundColor: Colors.white,
               ),
-              child: const Text(
+              icon: const Icon(Icons.arrow_right_alt),
+              label: const Text(
                 "Start Quiz",
               ))
         ],
