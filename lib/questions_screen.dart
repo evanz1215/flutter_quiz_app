dc754df3f7636fcee34ea46a2463c80476ddf3d7
Evan <evanz1215@gmail.com>
2024-03-23 22:24:22 +0800

    製作問答頁面part1

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -10,6 +10,27 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Questions Screen");
+    return SizedBox(
+      width: double.infinity, //盡可能填滿螢幕寬度
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question...'),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -20,21 +20,26 @@ class _QuizState extends State<Quiz> {
 
   @override
   Widget build(BuildContext context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 78, 13, 151),
+                  Color.fromARGB(255, 107, 15, 168),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
             ),
-          ),
-          child: activeScreen == 'start-screen' ? StartScreen(switchScreen) : const QuestionsScreen(),
-        ),
+            child: screenWidget),
       ),
     );
   }
