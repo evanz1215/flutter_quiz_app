e4b4d61e910c61af1780108191dedb856c57f860
Evan <evanz1215@gmail.com>
2024-03-23 15:46:58 +0800

    Refactor screen switching logic in Quiz widget

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -10,18 +10,11 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-
-    super.initState();
-  }
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
@@ -40,7 +33,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          child: activeScreen == 'start-screen' ? StartScreen(switchScreen) : const QuestionsScreen(),
         ),
       ),
     );
